17b3582ea0fca2b4e55a12827c16c61c122881a6
anniinaiida <kopsa.anniina@gmail.com>
2022-05-03 20:50:10 +0300

    uibuild

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fullstackopen_part3
+
+phonebook 3.1-3.6
+phonebook 3.7
+phonebook 3.9-3.11
+
+Heroku link: https://vast-dusk-76026.herokuapp.com/
--- index.js
+++ index.js
@@ -1,38 +1,47 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
+
 
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+  .then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
+
+  // response.json(persons)
 })
 
 app.get('/info', (request, response) => {
@@ -41,54 +50,97 @@ app.get('/info', (request, response) => {
   response.send(message)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error => next(error))
+
+    // const id = Number(request.params.id)
+    // const person = persons.find(person => person.id === id)
+
+    // if (person) {
+    //     response.json(person)
+    // } else {
+    //     response.status(404).end()
+    // }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
     response.status(204).end()
+  })
+  .catch(error => next(error))
+  
+  // const id = Number(request.params.id)
+  //   persons = persons.filter(person => person.id !== id)
+    
+  //   response.status(204).end()
 })
 
-const generateId = () => {
-    const randomId = Math.floor(Math.random() * 1000000)
-    return randomId
-}
+// const generateId = () => {
+//     const randomId = Math.floor(Math.random() * 1000000)
+//     return randomId
+// }
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
         return response.status(400).json({ 
         error: 'content missing' 
         })
-    } else if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({ 
-        error: 'name must be unique' 
-        })
-    }
+    } 
+    // else if (persons.find(person => person.name === body.name)) {
+    //     return response.status(400).json({ 
+    //     error: 'name must be unique' 
+    //     })
+    // }
 
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number
-    }
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
 
-    persons = persons.concat(person)
+    person.save()
+      .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 
-    response.json(person)
+    // const person = {
+    //   id: generateId(),
+    //   name: body.name,
+    //   number: body.number
+    // }
+
+    // persons = persons.concat(person)
+
+    // response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
